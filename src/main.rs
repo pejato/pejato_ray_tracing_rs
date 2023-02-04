ae9ff988a3eca48d8b9d66f76f3c6bb1bdc9fb2f
Peter Tolsma <pejato@umich.edu>
2023-02-04 12:43:09 -0600

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,24 @@
+
+fn header(width: i32, height: i32) -> String {
+    format!("P3\n{width} {height}\n255").into()
+}
+
+fn main() {
+    let (image_width, image_height) = (256, 256);
+    let header = header(image_width, image_height);
+    println!("{header}");
+
+    let pixel = |flt: f32| (flt * 255.999).round() as i32;
+    for j in (0..image_height).rev() {
+        eprintln!("Lines remaining {j}");
+        for i in 0..image_width {
+            let r = i as f32 / (image_width - 1) as f32;
+            let g = j as f32 / (image_height - 1) as f32;
+            let b = 0.25;
+
+            let (ir, ig, ib) = (pixel(r), pixel(g), pixel(b));
+            println!("{ir} {ig} {ib}");
+        }
+    }
+    eprintln!("Fin");
+}
